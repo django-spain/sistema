ff195327f6eadcec7e911b9681fb07bd8d117acd
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-02-17 09:26:26 +0100

    sc

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
 urlpatterns = [
     path('', views.inicio, name='Inicio'),
-]
+    path('nosotros', views.nosotros, name='Nosotros'),
+    
+    path('libros', views.libros, name='Libros'),
+    path('libros/crear', views.crear, name='Crear'),
+    path('eliminar/<int:id>', views.eliminar, name='Eliminar'),
+    path('libros/editar/<int:id>', views.editar, name='Editar'),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- sistema/settings.py
+++ sistema/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,6 +75,17 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'libreria',
+#         'USER' : 'root',
+#         'PASSWORD' : '',
+#         'HOST' : 'localhost',
+#         'PORT' : '3307'
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -119,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = '/imagenes/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
